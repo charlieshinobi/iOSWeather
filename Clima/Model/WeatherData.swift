62f7a96e2b7bd542711084f4814462b1c26de4e5
Charlie Ramirez <shinobi@Charlies-MacBook-Pro.local>
2020-10-01 21:01:13 -0500

    agregar lectura de json por api de weather y mostrarlo en pantalla dependiendo de la ciudad escrita

---
+++ Clima/Model/WeatherData.swift
@@ -0,0 +1,24 @@
+//
+//  WeatherData.swift
+//  Clima
+//
+//  Created by Charlie Ramirez on 01/10/20.
+//  Copyright © 2020 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherData: Codable {
+    let name: String
+    let main: Main
+    let weather: [Weather]
+}
+
+struct Main: Codable {
+    let temp: Double
+}
+
+struct Weather: Codable {
+    let description: String
+    let id: Int
+}
